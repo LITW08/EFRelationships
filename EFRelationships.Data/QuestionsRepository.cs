f52e7d9253d71240fa090c72c6fee9cf58250e1a
Avrumi Friedman <a.friedman07@gmail.com>
2021-04-07 21:53:18 -0400

    Add project files.

---
+++ EFRelationships.Data/QuestionsRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFRelationships.Data
+{
+    public class QuestionsRepository
+    {
+        private readonly string _connectionString;
+
+        public QuestionsRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private Tag GetTag(string name)
+        {
+            using var ctx = new PeopleCarsContext(_connectionString);
+            return ctx.Tags.FirstOrDefault(t => t.Name == name);
+        }
+
+        private int AddTag(string name)
+        {
+            using var ctx = new PeopleCarsContext(_connectionString);
+            var tag = new Tag { Name = name };
+            ctx.Tags.Add(tag);
+            ctx.SaveChanges();
+            return tag.Id;
+        }
+
+        public List<Question> GetQuestionsForTag(string name)
+        {
+            using var ctx = new PeopleCarsContext(_connectionString);
+            return ctx.Questions.Include(q => q.QuestionsTags)
+                .ThenInclude(qt => qt.Tag)
+                .Where(c => c.QuestionsTags.Any(t => t.Tag.Name == name))
+                .ToList();
+        }
+
+        public void AddQuestion(Question question, IEnumerable<string> tags)
+        {
+            using var ctx = new PeopleCarsContext(_connectionString);
+            ctx.Questions.Add(question);
+            ctx.SaveChanges();
+            foreach (string tag in tags)
+            {
+                Tag t = GetTag(tag);
+                int tagId;
+                if (t == null)
+                {
+                    tagId = AddTag(tag);
+                }
+                else
+                {
+                    tagId = t.Id;
+                }
+                ctx.QuestionsTags.Add(new QuestionsTags
+                {
+                    QuestionId = question.Id,
+                    TagId = tagId
+                });
+            }
+
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ EFRelationships.Web/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using EFRelationships.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using EFRelationships.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace EFRelationships.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString;
+        public HomeController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
+
+        public IActionResult Index()
+        {
+            var repo = new PeopleCarsRepository(_connectionString);
+            return View(repo.GetPeople());
+        }
+
+        public IActionResult NewPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            var repo = new PeopleCarsRepository(_connectionString);
+            repo.AddPerson(person);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult AddCar(int personId)
+        {
+            var repo = new PeopleCarsRepository(_connectionString);
+            var person = repo.GetPerson(personId);
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            var repo = new PeopleCarsRepository(_connectionString);
+            repo.AddCar(car);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Losers()
+        {
+            var repo = new PeopleCarsRepository(_connectionString);
+            return View(repo.GetPeopleWithNoCars());
+        }
+    }
+}
